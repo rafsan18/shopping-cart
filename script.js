00a916371893a9fa95abdc6815ee2ef58bbd719f
rafsan <rafsanamin18@gmail.com>
2020-07-24 08:36:51 +0600

    iPhone 11 quantity optimized

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Plus Button Event Handler iPhone 11
 const iPhone11AddBtn = document.getElementById("iPhone11AddBtn");
 iPhone11AddBtn.addEventListener("click", function () {
-  const inputQuantity = document.getElementById("iPhone11Quantity").value;
-  const inputQuantityNumber = parseInt(inputQuantity);
-  document.getElementById("iPhone11Quantity").value = inputQuantityNumber + 1;
+  //quantity
+  document.getElementById("iPhone11Quantity").value =
+    quantity("iPhone11Quantity") + 1;
 });
+
+// Minus Button Event Handler iPhone 11
+const iPhone11MinusBtn = document.getElementById("iPhone11minusBtn");
+iPhone11MinusBtn.addEventListener("click", function () {
+  //quantity
+  document.getElementById("iPhone11Quantity").value =
+    quantity("iPhone11Quantity") - 1;
+});
+
+// Plus Button Event Handler iPhone 11 Case
+// Minus Button Event Handler iPhone 11
+
+// function
+function quantity(id) {
+  const quantity = document.getElementById(id).value;
+  const quantityNumber = parseInt(quantity);
+  return quantityNumber;
+}
