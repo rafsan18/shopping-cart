f354eebb3bf5c6fd2a364487f41ca42681010379
rafsan <rafsanamin18@gmail.com>
2020-07-24 16:33:58 +0600

    iPhone 11 subtotal adjusted

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const iPhone11AddBtn = document.getElementById("iPhone11AddBtn");
 const iPhone11Price = document.getElementById("iPhone11Price").innerText;
 const iPhone11PriceNumber = parseFloat(iPhone11Price);
+const subTotal = document.getElementById("subTotal").innerText;
+const subTotalNumber = parseFloat(subTotal);
 iPhone11AddBtn.addEventListener("click", function () {
   //quantity
   const iPhoneQuantity = quantity("iPhone11Quantity") + 1;
@@ -11,6 +13,12 @@ iPhone11AddBtn.addEventListener("click", function () {
 
   const totalIndividualPrice = iPhone11PriceNumber * iPhoneQuantity;
   document.getElementById("iPhone11Price").innerText = totalIndividualPrice;
+
+  // increase in subtotal
+
+  const balanceSubTotal =
+    subTotalNumber + iPhone11PriceNumber * iPhoneQuantity - iPhone11PriceNumber;
+  document.getElementById("subTotal").innerText = balanceSubTotal;
 });
 
 // Minus Button Event Handler iPhone 11
@@ -24,6 +32,11 @@ iPhone11MinusBtn.addEventListener("click", function () {
 
   const totalIndividualPrice = iPhone11PriceNumber * iPhoneQuantity;
   document.getElementById("iPhone11Price").innerText = totalIndividualPrice;
+
+  // decrease in subtotal
+  const balanceSubTotal =
+    subTotalNumber + iPhone11PriceNumber * iPhoneQuantity - iPhone11PriceNumber;
+  document.getElementById("subTotal").innerText = balanceSubTotal;
 });
 
 // Plus Button Event Handler iPhone 11 Case
@@ -40,6 +53,8 @@ iPhone11CaseAddBtn.addEventListener("click", function () {
   // Increase in total individual price
   const totalIndividualPrice = iPhone11CasePriceNumber * iPhoneCaseQuantity;
   document.getElementById("iPhone11CasePrice").innerText = totalIndividualPrice;
+
+  // increase in subtotal
 });
 
 // Minus Button Event Handler iPhone 11 Case
